4d2fd9b9cc2b156b6d048bc58bd7be3938a058d9
Mystaken <tianxiang.gao.1996@gmail.com>
2018-03-14 14:05:47 -0400

    Added validator

--- api/lib/spec.js
+++ api/lib/spec.js
@@ -1,14 +1,22 @@
 'use strict';
 
 var express = require('./express'),
-  mongoose  = require('./mongoose');
+  mongoose  = require('./mongoose'),
+  validator = require('./validator'),
+  logger    = require('./logger');
 
 
 module.exports = function(app) {
   return {
     configure: function (opt) {
       return mongoose.configure(app, opt).then(function() {
+        logger.info('Mongoose configured');
         return express.configure(app, opt);
+      }).then(function() {
+        logger.info('Express configured');
+        return validator.configure(app, opt);
+      }).then(function() {
+        logger.info('Validator configured');
       });
     },
     onconfig: function (config, next) {
---
+++ api/lib/validator.js
@@ -0,0 +1,44 @@
+'use strict';
+var Promise = require('bluebird'),
+  ZSchema   = require('z-schema'),
+  moment    = require('moment'),
+  opt       = {},
+  validator;
+
+
+
+ZSchema.registerFormat("date", function (date) {
+  return date && moment(date, 'YYYY-MM-DD', true).isValid();
+});
+
+validator = new ZSchema(opt);
+
+module.exports = {
+  getLastErrors: function(opt) {
+    var last_errors = validator.getLastErrors();
+    if (last_errors) {
+      return last_errors.map(function (err) {
+        var path;
+        if (err.code === 'OBJECT_ADDITIONAL_PROPERTIES') {
+          path = err.params[0].map(function(param){
+            return '#/' + param;
+          });
+        } else if (err.code === 'OBJECT_MISSING_REQUIRED_PROPERTY') {
+          path = [ '#/' + err.params[0] ];
+        } else {
+          path = [ err.path ];
+        }
+        return {
+          code: err.code,
+          param: path
+        };
+      });
+    }
+  },
+  validate: function(json, schema) {
+    return validator.validate(json, schema);
+  },
+  configure: function(opt) {
+    return Promise.resolve();
+  }
+};
