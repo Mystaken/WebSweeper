82222934e66211abe246bc4105a42c0e8972e8e6
Mystaken <tianxiang.gao.1996@gmail.com>
2018-03-18 02:21:20 -0400

    Changed the way to init sockets

--- api/sockets/sockets.js
+++ api/sockets/sockets.js
@@ -5,27 +5,30 @@ const chat = require('./chat.js'),
     path: '/socket'
   });
 
-module.exports = function(server, session) {
-  io.attach(server);
-  // get session
-  io.use(function(socket, next) {
-    session(socket.request, socket.request.res, next);
-  });
-
-  //check if authenicated
-  /*
-  io.use(function(socket, next) {
-    if (!socket.request.session.user_id) {
-      return next(new Error('authentication error'));
-    }
-    return next();
-  });*/
-
-  io.on('connection', function(socket) {
-    chat(io, socket);
-
-    socket.on('disconnect', function(){
-      console.log('user disconnected');
+module.exports = {
+  init: function(server, session) {
+    io.attach(server);
+    // get session
+    io.use(function(socket, next) {
+      session(socket.request, socket.request.res, next);
     });
-  });
+
+    //check if authenicated
+    /*
+    io.use(function(socket, next) {
+      if (!socket.request.session.user_id) {
+        return next(new Error('authentication error'));
+      }
+      return next();
+    });*/
+
+    io.on('connection', function(socket) {
+      chat(io, socket);
+
+      socket.on('disconnect', function(){
+        console.log('user disconnected');
+      });
+    });
+    this.io;
+  }
 }
